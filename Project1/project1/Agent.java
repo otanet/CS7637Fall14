bece660b70bcf15147d7d4bfdd5b93c46d6f01e7
wterrill <william.terrill@gmail.com>
2014-09-08 12:27:04 -0500

    made loops instead of hardcoding for agent

--- Project1/project1/Agent.java
+++ Project1/project1/Agent.java
@@ -1,6 +1,7 @@
 package project1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -56,119 +57,51 @@ public class Agent {
      * @return your Agent's answer to this problem
      */
     public String Solve(RavensProblem problem) {
-        //ArrayList<ObjectTrans>
-
-    	//Variables
+        //Variables
+    	ArrayList<ObjectTrans> transformations = new ArrayList<>();
+    	ObjectTrans goal;
     	String answer="0";
-    	//Tell console what you're doing
+    	ArrayList<String> options = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6"));
+    	
+    	
+    	
+    //Tell console what you're doing
     	System.out.println("Agent.java iteration = " + index + " Problem Name = " + problem.getName() + "");
     	
-    	//Tell console what pieces you're working with
+    //Get initial pieces and make the goal transformation difference array
     	HashMap<String, RavensFigure> figures = problem.getFigures();
     	RavensFigure A = figures.get("A");
-    	//System.out.println("Name = " + A.getName() + " number of objects inside frame = " + A.getObjects().size() );
     	RavensFigure B = figures.get("B");
     	
-    	ObjectTrans goal = new ObjectTrans(A.getObjects().get(0),B.getObjects().get(0));
+    	goal = new ObjectTrans(A.getObjects().get(0),B.getObjects().get(0));
     	System.out.println("-------GOAL-------");
     	for(int x=0; x<goal.getDiffArray().size(); x++)
     	{
     		System.out.println(goal.getDiffArray().get(x));
     	}
     	
+    //Get figure C and prep for looping through the 6 guesses.
     	RavensFigure C = figures.get("C");
     	
-    	RavensFigure oneFigure = figures.get("1");
-    	ObjectTrans first = new ObjectTrans(C.getObjects().get(0),oneFigure.getObjects().get(0));
+    	for(int guesses = 0; guesses < 6; guesses++)
+    	{
+    	//RavensFigure oneFigure = figures.get("1");
+    	ObjectTrans temp = new ObjectTrans(C.getObjects().get(0),figures.get(options.get(guesses)).getObjects().get(0));
+    	transformations.add(temp);
     	System.out.println("-------First Guess-------");
-    	for(int x=0; x<first.getDiffArray().size(); x++)
-    	{
-    		System.out.println(first.getDiffArray().get(x));
+    	for(int x=0; x<temp.getDiffArray().size(); x++)
+    		{
+    			System.out.println(temp.getDiffArray().get(x));
+    		}
     	}
     	
-    	
-    	RavensFigure twoFigure = figures.get("2");
-    	ObjectTrans second = new ObjectTrans(C.getObjects().get(0),twoFigure.getObjects().get(0));
-    	System.out.println("-------Second Guess-------");
-    	for(int x=0; x<second.getDiffArray().size(); x++)
+    	for(int guesses = 0; guesses < 6; guesses++)
     	{
-    		System.out.println(second.getDiffArray().get(x));
+    		if(goal.getDiffArray().equals(transformations.get(guesses).getDiffArray())==true)
+    		{
+    			answer = options.get(guesses);
+    		}
     	}
-    	
-    	RavensFigure threeFigure = figures.get("3");
-    	ObjectTrans third = new ObjectTrans(C.getObjects().get(0),threeFigure.getObjects().get(0));
-    	System.out.println("-------Third Guess-------");
-    	for(int x=0; x<third.getDiffArray().size(); x++)
-    	{
-    		System.out.println(third.getDiffArray().get(x));
-    	}
-    	
-    	RavensFigure fourFigure = figures.get("4");
-    	ObjectTrans fourth = new ObjectTrans(C.getObjects().get(0),fourFigure.getObjects().get(0));
-    	System.out.println("-------Fourth Guess-------");
-    	for(int x=0; x<fourth.getDiffArray().size(); x++)
-    	{
-    		System.out.println(fourth.getDiffArray().get(x));
-    	}
-    	
-    	RavensFigure fiveFigure = figures.get("5");
-    	ObjectTrans fifth = new ObjectTrans(C.getObjects().get(0),fiveFigure.getObjects().get(0));
-    	System.out.println("-------Fifth Guess-------");
-    	for(int x=0; x<fifth.getDiffArray().size(); x++)
-    	{
-    		System.out.println(fifth.getDiffArray().get(x));
-    	}
-    	
-    	RavensFigure sixFigure = figures.get("6");
-    	ObjectTrans sixth = new ObjectTrans(C.getObjects().get(0),sixFigure.getObjects().get(0));
-    	System.out.println("-------Sixth Guess-------");
-    	for(int x=0; x<sixth.getDiffArray().size(); x++)
-    	{
-    		System.out.println(sixth.getDiffArray().get(x));
-    	}
-    	
-    	if (goal.getDiffArray().equals(first.getDiffArray())==true)
-    	{
-    		answer = "1";
-    	}
-    	
-    	else if (goal.getDiffArray().equals(second.getDiffArray())==true)
-    	{
-    		answer = "2";
-    	}
-    	
-    	else if (goal.getDiffArray().equals(third.getDiffArray())==true)
-    	{
-    		answer = "3";
-    	}
-    	else if (goal.getDiffArray().equals(fourth.getDiffArray())==true)
-    	{
-    		answer = "4";
-    	}
-    	
-    	else if (goal.getDiffArray().equals(fifth.getDiffArray())==true)
-    	{
-    		answer = "5";
-    	}
-    	
-    	else if (goal.getDiffArray().equals(sixth.getDiffArray())==true)
-    	{
-    		answer = "6";
-    	}
-    	//goal.figureTransformation(figures.get("A"),figures.get("B"));
-
-        
-        
-        /*
-        HashMap<String, RavensFigure> test = problem.getFigures();
-        RavensFigure A = test.get("A");
-        ArrayList<RavensObject> object = A.getObjects();
-        RavensObject thingy = object.get(0);
-        String thingyName= thingy.getName();
-        System.out.println(thingyName);
-        
-        System.out.println(name);
-        */
     	
         index = index + 1;
         
